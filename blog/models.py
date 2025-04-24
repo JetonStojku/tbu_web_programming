5f2da376213a7521dc8c0626f5e6c7ea9fe34000
js-flowzar <jetonstojku@hotmail.com>
2025-04-25 01:09:40 +0200

    add, edit, delete posts and comments

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,9 +11,13 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+    
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     date_commented = models.DateTimeField(auto_now_add=True)
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,14 @@
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import get_object_or_404, redirect, render
+from django.urls import reverse
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
                                   UpdateView)
 
 from blog.forms import CommentForm, PostForm, UserRegisterForm
 
-from .models import Post
+from .models import Comment, Post
 
 
 class PostListView(ListView):
@@ -25,6 +27,11 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+    
+    def get_success_url(self):
+        if not hasattr(self, 'object') or not self.object.pk:
+            return reverse('home')  # fallback if something went wrong
+        return reverse('post-detail', kwargs={'pk': self.object.pk})
 
 class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
@@ -32,9 +39,17 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    success_url = '/'
+    
+    def get_success_url(self):
+        messages.success(self.request, 'Post has been deleted successfully!')
+        return reverse('home')
+    
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(author=self.request.user)
 
-def post_detail(request, pk):
+@login_required
+def add_comment(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == 'POST':
         comment_form = CommentForm(request.POST)
@@ -43,14 +58,21 @@ def post_detail(request, pk):
             comment.post = post
             comment.author = request.user
             comment.save()
-            return redirect('post-detail', pk=post.pk)
+            messages.success(request, 'Your comment has been added!')
+        else:
+            messages.error(request, 'Error adding your comment!')
+    return redirect('post-detail', pk=post.pk)
+
+@login_required
+def comment_delete(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    if request.user == comment.author:
+        comment.delete()
+        messages.success(request, 'Your comment has been deleted!')
     else:
-        comment_form = CommentForm()
-    
-    return render(request, 'blog/post_detail.html', {
-        'post': post,
-        'comment_form': comment_form
-    })
+        messages.error(request, "You can't delete this comment!")
+    return redirect('post-detail', pk=post_pk)
 
 def register(request):
     if request.method == 'POST':
